e844b3bb0bacc3842360fe0b28b0b91abd004e5f
Andrii Ovdiienko <aovdiien@softserveinc.com>
2017-11-16 17:33:27 +0200

    created first redux Todo app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer'
+import AddTodo from './containers/AddTodo'
+import VisibleTodoList from './containers/VisibleTodoList'
+
 
 class App extends Component {
   render() {
@@ -13,6 +17,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <AddTodo />
+          <VisibleTodoList />
+          <Footer />
+        </div>
       </div>
     );
   }
